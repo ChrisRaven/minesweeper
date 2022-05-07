69dd8c72c9151efade24c21789b7e151c39a5fc6
KrzysztofKruk <kr.kruk@gmail.com>
2022-05-07 20:40:16 +0200

    Move anything related to playfield to separate class and module

---
+++ Playfield.js
@@ -0,0 +1,229 @@
+import { settings, ICON } from './index.js'
+
+const DIRECTION = {
+  ADD: 0,
+  SUBTRACT: 1
+}
+
+export const STATE = {
+  HIDDEN: 0,
+  VISIBLE: 1,
+  FLAGGED: 2
+}
+
+export default class Playfield {
+  constructor () {
+    this.playfield = []
+    this.DOMNode = document.getElementById('playfield')
+  }
+
+  generate() {  
+    let x = settings.x
+    let y = settings.y
+    const tiles = []
+
+    this.playfield = Array(x).fill(null).map(() => Array(y))
+
+    for (let i = 0; i < x; i++) {
+      for (let j = 0; j < y; j++) {
+        this.playfield[i][j] = { content: null, state: null }
+        tiles.push(`<div class="tile" id="${i}-${j}"> </div>`)
+      }
+      tiles.push('<br />')
+    }
+
+    // to remove previous event listeners
+    this.DOMNode.replaceWith(this.DOMNode.cloneNode(true))
+    this.DOMNode = document.getElementById('playfield')
+
+    this.DOMNode.innerHTML = tiles.join('')
+  }
+
+  getField(arg1, arg2) {
+    let x, y
+
+    if (typeof arg1 === 'object') {
+      x = arg1.x
+      y = arg1.y
+    }
+    else {
+      x = arg1
+      y = arg2
+    }
+    return this.playfield[x][y]
+  }
+
+  placeMines() {
+    let x = settings.x
+    let y = settings.y
+    let mines = settings.mines
+    const size = x * y
+    
+    while (mines) {
+      let position = Math.floor(Math.random() * size)
+      let px = Math.floor(position / y);
+      let py = position % y
+
+      let field = this.getField(px, py)
+      field.state = STATE.HIDDEN
+
+      if (field.content === 'mine') continue
+
+      field.content = 'mine'
+      mines--
+    }
+  }
+
+  getNeighboursCoords(x, y, adjacentOnly =  false) {
+    let maxX = settings.x
+    let maxY = settings.y
+    
+    /*
+    p = plus
+    m = minus
+
+    xm1my xmy xp1my
+    xm1    x    xp1
+    xm1py xpy xp1py
+    */
+
+    let result = {
+      xm1:   x > 0        ? { x: x - 1, y: y     } : null,
+      xp1:   x < maxX - 1 ? { x: x + 1, y: y     } : null,
+      xmy:   y > 0        ? { x: x    , y: y - 1 } : null,
+      xpy:   y < maxY - 1 ? { x: x    , y: y + 1 } : null
+    }
+
+    if (!adjacentOnly) {
+      result = Object.assign(result, {
+        xm1my: x > 0        && y > 0        ? { x: x - 1, y: y - 1 } : null,
+        xm1py: x > 0        && y < maxY - 1 ? { x: x - 1, y: y + 1 } : null,
+        xp1my: x < maxX - 1 && y > 0        ? { x: x + 1, y: y - 1 } : null,
+        xp1py: x < maxX - 1 && y < maxY - 1 ? { x: x + 1, y: y + 1 } : null
+      })
+    }
+
+    return result
+  }
+
+
+  calculateNeighbours() {
+    let x = settings.x
+    let y = settings.y
+
+    for (let i = 0; i < x; i++) {
+      for (let j = 0; j < y; j++) {
+        let field = this.getField(i, j)
+
+        field.state = STATE.HIDDEN
+        if (field.content === 'mine') continue
+
+        let neighbours = this.getNeighboursCoords(i, j)
+        let counter = 0
+
+        for (let coords of Object.values(neighbours)) {
+          if (coords === null) continue
+
+          let el = this.getField(coords);
+          if (el.content === 'mine') {
+            counter++
+          }
+        }
+
+        field.content = counter
+      }
+    }
+  }
+
+  getColorClass(content) {
+    switch (content) {
+      case 0: return ''
+      case 1: return 'one'
+      case 2: return 'two'
+      case 3: return 'three'
+      case 4: return 'four'
+      case 5: return 'five'
+      case 6: return 'six'
+      case 7: return 'seven'
+      case 8: return 'eight'
+      case 'mine': return ''
+    }
+  }
+
+
+  uncoverTile({ x, y }) {
+    let field = this.getField(x, y)
+    field.state = STATE.VISIBLE
+
+    let value = field.content === 0 ? '' : field.content
+    
+    let element = document.getElementById(x + '-' + y)
+    element.textContent = value
+    element.classList.add(this.getColorClass(value) || null)
+    element.classList.add('opened-tile')
+  }
+
+
+  uncoverNeighbours({ x, y }) {
+    this.uncoverTile({ x, y })
+    let neighbours = this.getNeighboursCoords(x, y, true)
+    for (let neighbour of Object.values(neighbours)) {
+      if (neighbour === null) continue
+
+      let field = this.getField(neighbour);
+      if (field.state === STATE.HIDDEN) {
+        if (field.content === 0) {
+          this.uncoverNeighbours(neighbour)
+        }
+        else if (field.content !== 'mine' && field.state !== STATE.FLAGGED) {
+          this.uncoverTile(neighbour)
+        }
+      }
+    }
+  }
+
+
+  uncover() {
+    let x = settings.x
+    let y = settings.y
+
+    for (let i = 0; i < x; i++) {
+      for (let j = 0; j < y; j++) {
+        let tile = this.getField(i, j)
+        let text = tile.content
+
+        if (tile.content === 'mine' && tile.state === STATE.FLAGGED) {
+          text = ICON.FLAG
+        }
+        if (tile.content === 'mine' && tile.state !== STATE.FLAGGED) {
+          text = ICON.MINE
+        }
+        else if (tile.content !== 'mine' && tile.state === STATE.FLAGGED) {
+          text = 'F'
+        }
+        else if (tile.content === 0) {
+          text = ''
+        }
+
+        document.getElementById(i + '-' + j).textContent = text
+      }
+    }
+  }
+
+
+  checkIfWon() {
+    let correctlyMarkedMines = 0
+
+    for (let i = 0; i < settings.x; i++) {
+      for (let j = 0; j < settings.y; j++) {
+        let el = playfield[i][j]
+        if (el.state === STATE.FLAGGED && el.content === 'mine') {
+          correctlyMarkedMines++
+        }
+      }
+    }
+
+    return correctlyMarkedMines === settings.mines
+  }
+
+}
--- index.js
+++ index.js
@@ -1,39 +1,22 @@
 import Time from './Time.js'
 import Settings from './Settings.js'
+import Playfield, { STATE } from './Playfield.js'
 
 
-let playfield = []
 let gameEnded = false
 let numberOfFlagsPlaced = 0
 
 
 let time = new Time()
-let settings = new Settings()
+export let settings = new Settings()
+let playfield = new Playfield()
 
 
-const DIRECTION = {
-  ADD: 0,
-  SUBTRACT: 1
-}
-
-const STATE = {
-  HIDDEN: 0,
-  VISIBLE: 1,
-  FLAGGED: 2
-}
-
-
-const ICON = {
+export const ICON = {
   WON_FACE: '\u{1F600}',
   LOST_FACE: '\u{1F61E}',
   FLAG: '\u{1F6A9}',
   MINE: '\u{1F4A3}'
-}
-
-
-// shortcut for cases, when both x and y are from the same object
-function getField({ x, y }) {
-  return playfield[x][y]
 }
 
 
@@ -50,131 +33,10 @@ function startGame() {
   numberOfFlagsPlaced = 0
   time.resetTimer()
   updateNumberOfFlags()
-  generatePlayfield()
-  placeMines()
-  calculateNeighbours()
-}
-
-
-function generatePlayfield() {  
-  let x = settings.x
-  let y = settings.y
-  let playfieldElement = document.getElementById('playfield')
-  const tiles = []
-
-  playfield = Array(x).fill(null).map(() => Array(y))
-
-  for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++) {
-      playfield[i][j] = { content: null, state: null }
-      tiles.push(`<div class="tile" id="${i}-${j}"> </div>`)
-    }
-    tiles.push('<br />')
-  }
-
-  // to remove previous event listeners
-  playfieldElement.replaceWith(playfieldElement.cloneNode(true))
-  playfieldElement = document.getElementById('playfield')
-
-  playfieldElement.innerHTML = tiles.join('')
-  playfieldElement.addEventListener('click', handleLeftClickOnTile.bind(event))
-  playfieldElement.addEventListener('contextmenu', handleRightClickOnTile.bind(event))
-}
-
-
-function placeMines() {
-  let x = settings.x
-  let y = settings.y
-  let mines = settings.mines
-  const size = x * y
-  
-  while (mines) {
-    let position = Math.floor(Math.random() * size)
-    let px = Math.floor(position / y);
-    let py = position % y
-
-    let field = getField({ x: px, y: py })
-    field.state = STATE.HIDDEN
-
-    if (field.content === 'mine') continue
-
-    field.content = 'mine'
-    mines--
-  }
-}
-
-function getNeighboursCoords(x, y, adjacentOnly =  false) {
-  let maxX = settings.x
-  let maxY = settings.y
-  
-  /*
-  p = plus
-  m = minus
-
-  xm1my xmy xp1my
-  xm1    x    xp1
-  xm1py xpy xp1py
-  */
-
-  let result = {
-    xm1:   x > 0        ? { x: x - 1, y: y     } : null,
-    xp1:   x < maxX - 1 ? { x: x + 1, y: y     } : null,
-    xmy:   y > 0        ? { x: x    , y: y - 1 } : null,
-    xpy:   y < maxY - 1 ? { x: x    , y: y + 1 } : null
-  }
-
-  if (!adjacentOnly) {
-    result = Object.assign(result, {
-      xm1my: x > 0        && y > 0        ? { x: x - 1, y: y - 1 } : null,
-      xm1py: x > 0        && y < maxY - 1 ? { x: x - 1, y: y + 1 } : null,
-      xp1my: x < maxX - 1 && y > 0        ? { x: x + 1, y: y - 1 } : null,
-      xp1py: x < maxX - 1 && y < maxY - 1 ? { x: x + 1, y: y + 1 } : null
-    })
-  }
-
-  return result
-}
-
-
-function calculateNeighbours() {
-  let x = settings.x
-  let y = settings.y
-
-  for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++) {
-      playfield[i][j].state = STATE.HIDDEN
-      if (playfield[i][j].content === 'mine') continue
-
-      let neighbours = getNeighboursCoords(i, j)
-      let counter = 0
-
-      for (let coords of Object.values(neighbours)) {
-        if (coords === null) continue
-
-        let el = getField(coords);
-        if (el.content === 'mine') {
-          counter++
-        }
-      }
-
-      playfield[i][j].content = counter
-    }
-  }
-}
-
-function getColorClass(content) {
-  switch (content) {
-    case 0: return ''
-    case 1: return 'one'
-    case 2: return 'two'
-    case 3: return 'three'
-    case 4: return 'four'
-    case 5: return 'five'
-    case 6: return 'six'
-    case 7: return 'seven'
-    case 8: return 'eight'
-    case 'mine': return ''
-  }
+  playfield.generate()
+  playfield.placeMines()
+  playfield.calculateNeighbours()
+  addEvents()
 }
 
 
@@ -188,7 +50,7 @@ function handleLeftClickOnTile(event) {
     const coords = {};
 
     [coords.x, coords.y] = clicked.id.split('-').map(el => parseInt(el, 10))
-    let field = getField(coords)
+    let field = playfield.getField(coords)
 
     if (field.state === STATE.FLAGGED) {
       updateNumberOfFlags(DIRECTION.SUBTRACT)
@@ -198,27 +60,13 @@ function handleLeftClickOnTile(event) {
       case 'mine':
         return loseGame(coords)
       case 0:
-        return uncoverNeighbours(coords)
+        return playfield.uncoverNeighbours(coords)
       default:
-        return uncoverTile(coords)
+        return playfield.uncoverTile(coords)
     }
   }
 }
 
-function checkIfWon() {
-  let correctlyMarkedMines = 0
-
-  for (let i = 0; i < settings.x; i++) {
-    for (let j = 0; j < settings.y; j++) {
-      let el = playfield[i][j]
-      if (el.state === STATE.FLAGGED && el.content === 'mine') {
-        correctlyMarkedMines++
-      }
-    }
-  }
-
-  return correctlyMarkedMines === settings.mines
-}
 
 function handleRightClickOnTile(event) {
   event.preventDefault()
@@ -232,7 +80,7 @@ function handleRightClickOnTile(event) {
 
     [coords.x, coords.y] = clicked.id.split('-').map(el => parseInt(el, 10))
 
-    let field = getField(coords)
+    let field = playfield.getField(coords)
     if (field.state === STATE.HIDDEN) {
       clicked.textContent = ICON.FLAG
       field.state = STATE.FLAGGED
@@ -253,81 +101,24 @@ function handleRightClickOnTile(event) {
 }
 
 
-function uncoverTile({ x, y }) {
-  let element = document.getElementById(x + '-' + y)
-  let value = playfield[x][y].content
-  if (value === 0) {
-    value = ''
-  }
-  element.textContent = value
-  element.classList.add(getColorClass(value) || null)
-  playfield[x][y].state = STATE.VISIBLE
-  element.classList.add('opened-tile')
-}
-
-
-function uncoverNeighbours({ x, y }) {
-  uncoverTile({ x, y })
-  let neighbours = getNeighboursCoords(x, y, true)
-  for (let neighbour of Object.values(neighbours)) {
-    if (neighbour === null) continue
-
-    let field = getField(neighbour);
-    if (field.state === STATE.HIDDEN) {
-      if (field.content === 0) {
-        uncoverNeighbours(neighbour)
-      }
-      else if (field.content !== 'mine' && field.state !== STATE.FLAGGED) {
-        uncoverTile(neighbour)
-      }
-    }
-  }
-}
-
 function loseGame({ x, y }) {
   console.log('loser')
   document.getElementById('result-icon').textContent = ICON.LOST_FACE;
   document.getElementById(x + '-' + y).classList.add('exploded-tile')
-  showPlayfield()
+  playfield.uncover()
   time.stopTimer()
   gameEnded = true
 }
+
 
 function winGame() {
   document.getElementById('result-icon').textContent = ICON.WON_FACE;
   console.log('winner')
-  showPlayfield()
+  playfield.uncover()
   time.stopTimer()
   gameEnded = true
 }
 
-
-function showPlayfield() {
-  let x = settings.x
-  let y = settings.y
-
-  for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++) {
-      let tile = playfield[i][j]
-      let text = tile.content
-
-      if (tile.content === 'mine' && tile.state === STATE.FLAGGED) {
-        text = ICON.FLAG
-      }
-      if (tile.content === 'mine' && tile.state !== STATE.FLAGGED) {
-        text = ICON.MINE
-      }
-      else if (tile.content !== 'mine' && tile.state === STATE.FLAGGED) {
-        text = 'F'
-      }
-      else if (tile.content === 0) {
-        text = ''
-      }
-
-      document.getElementById(i + '-' + j).textContent = text
-    }
-  }
-}
 
 function handleRestartButton() {
   if (!gameEnded) {
@@ -340,10 +131,15 @@ function handleRestartButton() {
   }
 }
 
+function addEvents() {
+  playfield.DOMNode.addEventListener('click', handleLeftClickOnTile.bind(event))
+  playfield.DOMNode.addEventListener('contextmenu', handleRightClickOnTile.bind(event))
+}
 
 (() => {
   document.getElementById('confirm-parameters').addEventListener('click', () => { settings.saveSettings(); startGame() })
   document.getElementById('restart-button').addEventListener('click', handleRestartButton)
+  addEvents()
   settings.getSettings()
   startGame()
 })()
